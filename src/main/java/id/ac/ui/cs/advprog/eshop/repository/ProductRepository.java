e8a361c593225a6e5f4ac0c4ca4b5bb6f85e0e7e
Fathan <fathan.naufal@ui.ac.id>
2024-02-05 22:30:29 +0700

    fix: negative quantity bug

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -12,6 +12,9 @@ public class ProductRepository{
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product){
+        if(product.getProductQuantity() < 0){
+            product.setProductQuantity(0);
+        }
         productData.add(product);
         return product;
     }
