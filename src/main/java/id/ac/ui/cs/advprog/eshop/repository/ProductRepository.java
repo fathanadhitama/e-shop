425ab1dab251569bb047f544480146d3beaae6c2
Fathan <fathan.naufal@ui.ac.id>
2024-02-05 19:23:21 +0700

    add: edit product repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -10,10 +10,35 @@ import java.util.List;
 @Repository
 public class ProductRepository{
     private List<Product> productData = new ArrayList<>();
+    private int idCounter = 0;
 
     public Product create(Product product){
         productData.add(product);
         return product;
+    }
+
+    public Product findById(String id){
+        for(Product product: productData){
+            if(product.getProductId().equals(id)){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product edit(Product updatedProduct){
+        for (int i = 0; i < productData.size(); i++) {
+            Product product = productData.get(i);
+            if(product.getProductId().equals(updatedProduct.getProductId())){
+                productData.set(i, updatedProduct);
+                return updatedProduct;
+            }
+        }
+        return null;
+    }
+
+    public String generateID(){
+        return Integer.toString(idCounter++);
     }
 
     public Iterator<Product> findAll(){
