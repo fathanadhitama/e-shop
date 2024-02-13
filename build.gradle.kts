20a3b39d259fc4a86cb0e6813e0ba2ebf882226f
Fathan <fathan.naufal@ui.ac.id>
2024-02-13 22:10:21 +0700

    add: sonarcloud workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,15 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "fathanadhitama_e-shop")
+		property("sonar.organization", "fathanadhitama")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
 
 group = "id.ac.ui.cs.advprog"
